0fd5a8de492ad2902c9f38c2000cee652ede7b1a
Ivan <vsem2004@gmail.com>
2020-08-07 13:41:00 +0300

    M2T1 Creating modules

--- src/main.js
+++ src/main.js
@@ -1,91 +1,12 @@
+import {createUserRankTemplate} from "./view/user-rank";
+import {createMenuTemplate} from "./view/menu";
+import {createContainerForFilmsTemplate} from "./view/container";
+import {createCardForAllMoviesTemplate} from "./view/card";
+import {createButtonShowMoreTemplate} from "./view/button-load";
+import {createfooterStatisticTemplate} from "./view/footer-statistic";
+
 const NUMBER_OF_FILMS = 5;
 const NUMBER_OF_EXTRA_FILMS = 2;
-
-const createUserRankTemplate = () => {
-  return (
-    `<section class="header__profile profile">
-    <p class="profile__rating">Movie Buff</p>
-    <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
-  </section>`
-  );
-};
-
-const createSiteMenuTemplate = () => {
-  return (
-    `<nav class="main-navigation">
-    <div class="main-navigation__items">
-      <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
-      <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
-      <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
-      <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
-    </div>
-    <a href="#stats" class="main-navigation__additional">Stats</a>
-  </nav>
-  <ul class="sort">
-    <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-    <li><a href="#" class="sort__button">Sort by date</a></li>
-    <li><a href="#" class="sort__button">Sort by rating</a></li>
-  </ul>`
-  );
-};
-
-const createContainerForFilmsTemplate = () => {
-  return (
-    `<section class="films">
-      <section class="films-list">
-        <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
-
-        <div class="films-list__container"></div>
-      </section>
-      <section class="films-list--extra">
-        <h2 class="films-list__title">Top rated</h2>
-
-        <div class="films-list__container"></div>
-      </section>
-      <section class="films-list--extra">
-      <h2 class="films-list__title">Most commented</h2>
-
-      <div class="films-list__container"></div>
-      </section>
-    </section>`
-  );
-};
-
-const createCardForAllMoviesTemplate = () => {
-  return (
-    `<article class="film-card">
-          <h3 class="film-card__title">The Dance of Life</h3>
-          <p class="film-card__rating">8.3</p>
-          <p class="film-card__info">
-            <span class="film-card__year">1929</span>
-            <span class="film-card__duration">1h 55m</span>
-            <span class="film-card__genre">Musical</span>
-          </p>
-          <img src="./images/posters/the-dance-of-life.jpg" alt="" class="film-card__poster">
-          <p class="film-card__description">Burlesque comic Ralph "Skid" Johnson (Skelly), and specialty dancer Bonny Lee King (Carroll), end up together on a cold, rainy night at a tr…</p>
-          <a class="film-card__comments">5 comments</a>
-          <form class="film-card__controls">
-            <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
-            <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
-            <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
-          </form>
-        </article>`
-  );
-};
-
-const createButtonShowMoreTemplate = () => {
-  return (
-    `<button class="films-list__show-more">Show more</button>`
-  );
-};
-
-const createfooterStatisticTemplate = () => {
-  return (
-    `<section class="footer__statistics">
-    <p>130 291 movies inside</p>
-  </section>`
-  );
-};
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -95,7 +16,7 @@ const header = document.querySelector(`header`);
 const main = document.querySelector(`main`);
 
 render(header, createUserRankTemplate(), `beforeend`);
-render(main, createSiteMenuTemplate(), `beforeend`);
+render(main, createMenuTemplate(), `beforeend`);
 render(main, createContainerForFilmsTemplate(), `beforeend`);
 
 const filmList = main.querySelector(`.films-list`);
